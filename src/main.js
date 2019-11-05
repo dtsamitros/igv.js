4a86f0d9778c9d41c51a5ba4b88111f675f346c1
Dimitris Tsamitros <dst@alternet.gr>
2019-11-05 18:49:51 +0200

    Export FeatureParser

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@
 import FeatureFileReader from "../js/feature/featureFileReader";
 import FeatureSource from "../js/feature/featureSource";
 import GenomeUtils from "../js/genome/genome";
+import FeatureParser from "../js/feature/featureParsers"
 
 export {
 	FeatureFileReader,
 	FeatureSource,
 	GenomeUtils,
-};
+	FeatureParser,
+}

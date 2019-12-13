83022d5ce03505c170694ffd00d68d6f9c52f649
Dimitris Tsamitros <dst@alternet.gr>
2019-12-13 17:44:57 +0200

    Export igvxhr

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@
 import FeatureFileReader from "../js/feature/featureFileReader";
 import FeatureSource from "../js/feature/featureSource";
 import GenomeUtils from "../js/genome/genome";
-import FeatureParser from "../js/feature/featureParsers"
+import FeatureParser from "../js/feature/featureParsers";
+import igvxhr from "../js/igvxhr";
 
 export {
 	FeatureFileReader,
 	FeatureSource,
 	GenomeUtils,
 	FeatureParser,
+	igvxhr,
 }
